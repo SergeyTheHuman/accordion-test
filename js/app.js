ffeb6efd14c4a4f8d34c86560f90e29bbfd5ae13
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-05-29 13:22:16 +0300

    created accordion

--- js/app.js
+++ js/app.js
@@ -2,3 +2,29 @@ import { isWebp } from './components/isWebp.js'
 
 isWebp()
 
+const $accordionItems = document.querySelectorAll('.accordion__item')
+const $accordionContents = document.querySelectorAll('.accordion__content')
+
+$accordionItems.forEach((item) => {
+	let opened = false
+	const $accordionContent = item.querySelector('.accordion__content')
+	const $accordionTitle = item.querySelector('.accordion__title')
+	const accordionItemHeight = item.offsetHeight
+	const accordionContentHeight = $accordionContent.offsetHeight
+	item.addEventListener('click', (e) => {
+		if ($accordionContent) {
+			!opened ? openAccordion(item) : closeAccordion(item)
+		}
+	})
+
+	function openAccordion(accordionItem) {
+		accordionItem.style.height = `${accordionItemHeight + accordionContentHeight}px`
+		$accordionTitle.classList.add('active')
+		opened = true
+	}
+	function closeAccordion(accordionItem) {
+		accordionItem.style.height = `${accordionItemHeight}px`
+		$accordionTitle.classList.remove('active')
+		opened = false
+	}
+})
